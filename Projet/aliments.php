193efd33ccbc04d3ff39d3520635450a7be06af0
lebonhomme17 <antoinelepetitjean@gmail.com>
2018-11-13 08:16:46 +0100

    Add files via upload

--- Projet/aliments.php
+++ Projet/aliments.php
@@ -1,8 +1,53 @@
+<!DOCTYPE html>
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    include 'Donnees.inc.php';
+    
+    $aliment = $_GET['aliment'];
+    if (strcmp($aliment, '')==0){
+        $aliment='Aliment';
+    }
+    
+    $super = $Hierarchie[$aliment]['super-categorie'][0];
+    $sous = $Hierarchie[$aliment]['sous-categorie'];
+    
+?>
 
+<html>
+    <head>
+        <title>Aliments</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        
+        <?php
+            include 'menu.html';
+        ?>
+        
+        <div>
+            
+            <a href='aliments.php?aliment=<?php echo $super;?>'> <-- <?php echo $super;?> </a>
+            <h1><?php echo $aliment; ?></h1>
+            
+            <?php
+                    if(count($sous)==0){
+                        ?>
+            <a href="">voir les recettes</a>
+            <?php
+                    }
+                    else{
+                        foreach ($sous as $s){
+                        ?>
+            <a href="aliments.php?aliment=<?php echo $s;?>"><?php echo $s;?></a>
+            <?php
+                        }
+                    }
+            
+            ?>
+            
+            
+        </div>
+        
+    </body>
+</html>
--- Projet/recettes.php
+++ Projet/recettes.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <?php
-
     include 'Donnees.inc.php';
     
 ?>
